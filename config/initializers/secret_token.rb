b7e4f6dcb56786fe028d3765000a3e012a4bb7f4
avdotia <lauraperezfelipe@gmail.com>
2012-06-17 00:17:22 +0100

    First steps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miweb::Application.config.secret_token = '70652d7f94f9e3307f17262396dfed19058909ef1659c372a7cab76b5beda3166bcfd91cd3368bc5ffc9e543dc7fa6ba0ef6e56a6916138eb20b703d6d5a2628'
